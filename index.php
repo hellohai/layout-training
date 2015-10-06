6e21a742cffe7e0b8ffba3bba9c4361bc0689d46
James Jackson <james@jacksonjd.com>
2015-10-06 05:20:49 -0400

    layout training
    Simple training tool for teaching a friend of mine some basic CSS layout

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<body>
+  <head>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <div class="section section-header">
+    <div class="wrapper">
+      <img src="http://placehold.it/200x80" alt="" />
+      <a class="button" href="#">Sign Up</a>
+      </div>
+    </div>
+    <div class="section section-hero">
+      <div class="wrapper">
+        <img src="http://placehold.it/1200x200" alt="" />
+      </div>
+    </div>
+    <div class="section section-content">
+      <div class="wrapper">
+      <div class="content"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, aperiam, nesciunt! Voluptas nihil, deleniti! Expedita iusto odit repellat vitae, quo corporis quis harum, saepe, hic voluptates quod ipsa molestiae perferendis!</p></div>
+      <div class="image"><img src="http://placehold.it/350x150" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
